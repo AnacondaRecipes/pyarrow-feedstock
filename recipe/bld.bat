07a60306ea7d30769ff0a9a341735a598501ee2a
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2024-02-03 00:38:18 +0100

    enable acero

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,15 +6,18 @@ del cmake_modules\SetupCxxFlags.cmake
 del cmake_modules\CompilerInfo.cmake
 del cmake_modules\FindNumPy.cmake
 del cmake_modules\FindPythonLibsNew.cmake
+del cmake_modules\ArrowDatasetConfig.cmake
 copy /Y "%SRC_DIR%\cpp\cmake_modules\BuildUtils.cmake" cmake_modules\
 copy /Y "%SRC_DIR%\cpp\cmake_modules\SetupCxxFlags.cmake" cmake_modules\
 copy /Y "%SRC_DIR%\cpp\cmake_modules\CompilerInfo.cmake" cmake_modules\
 copy /Y "%SRC_DIR%\cpp\cmake_modules\FindNumPy.cmake" cmake_modules\
 copy /Y "%SRC_DIR%\cpp\cmake_modules\FindPythonLibsNew.cmake" cmake_modules\
+copy /Y "%SRC_DIR%\cpp\cmake_modules\ArrowDatasetConfig.cmake" cmake_modules\
 
 SET ARROW_HOME=%LIBRARY_PREFIX%
 SET SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
 SET PYARROW_BUILD_TYPE=release
+SET PYARROW_WITH_ACERO=1
 SET PYARROW_WITH_COMPUTE=1
 SET PYARROW_WITH_CSV=1
 SET PYARROW_WITH_S3=1
@@ -27,8 +30,6 @@ SET PYARROW_WITH_JSON=1
 SET PYARROW_WITH_ORC=1
 SET PYARROW_WITH_PARQUET=1
 SET PYARROW_WITH_PARQUET_ENCRYPTION=1
-@REM Plasma doesn't actually get built for Windows in arrow
-SET PYARROW_WITH_PLASMA=0
 SET PYARROW_CMAKE_GENERATOR=Ninja
 SET PYARROW_WITH_CUDA=0
 @rem manually specify the path to the cmake config files
